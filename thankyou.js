76e92e1f439e63f753cf1f929ff8e99fcc8d883c
geo <106122043+cloak-codes@users.noreply.github.com>
2023-03-25 11:15:11 -0700

    Add files via upload

---
+++ thankyou.js
@@ -0,0 +1,46 @@
+// background-blob
+
+const blob = document.getElementById("blob");
+document.body.onmousemove = (event) => {
+  const { clientX, clientY } = event;
+
+  blob.animate(
+    {
+      left: `${clientX}px`,
+      top: `${clientY}px`,
+    },
+    { duration: 5000, fill: "forwards" }
+  );
+};
+
+//glitchy text effect
+
+function letterScramble(event) {
+  let iteration = 1;
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return event.target.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= event.target.dataset.value.length) {
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 3;
+  }, 30);
+}
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector("h1").onmouseover = letterScramble;
