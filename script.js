44dd20d707ed405d3b9fc4392f3ee44e348c2922
geo <106122043+cloak-codes@users.noreply.github.com>
2023-04-19 08:30:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -47,3 +47,39 @@ document.querySelector("h1").onmouseover = letterScramble;
 document.getElementById("big-text").onmouseover = letterScramble;
 document.getElementById("contact-me-title").onmouseover = letterScramble;
 document.getElementById("projects-title").onmouseover = letterScramble;
+
+
+// Dark Mode
+
+const darkModeButton = document.querySelector(".dark-mode-button");
+const body = document.querySelector("body");
+const moonIcon = document.querySelector(".fa-moon");
+const links = document.querySelectorAll(".links");
+
+links.forEach((link) => {
+  link.style.color = "#313334";
+});
+
+darkModeButton.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+  if (body.classList.contains("dark-mode")) {
+    body.style.backgroundColor = "#313334";
+    body.style.Color = "#faf9f6";
+    document.getElementById("dark-mode-btn").style.color = "#faf9f6";
+    document.getElementById("dark-mode-btn").style.backgroundColor = "#313334";
+    document.getElementById("dark-mode-btn").style.borderColor = "#faf9f6";
+    links.forEach((link) => {
+      link.style.color = "#faf9f6";
+    });
+  } else {
+    body.style.backgroundColor = "#faf9f6";
+    body.style.Color = "#313334";
+    document.getElementById("dark-mode-btn").style.color = "#313334";
+    document.getElementById("dark-mode-btn").style.backgroundColor = "#faf9f6";
+    document.getElementById("dark-mode-btn").style.borderColor = "#313334";
+    links.forEach((link) => {
+      link.style.color = "#313334";
+    });
+  }
+});
+
